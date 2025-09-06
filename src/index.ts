b97ac658e790d53367b58268e3ee48fd8446e85b
AmritHehe <hopesalive1506@gmail.com>
2025-09-06 16:57:06 +0530

    added kafka , and wrote completed open and close endpoints
     yesterday + todays work , be communication using kafka ques also work

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,66 @@ const app = express() ;
 const users  :any = []
 import jwt from 'jsonwebtoken'
 import { Resend } from 'resend';
-import axios from 'axios'
-import { createClient } from 'redis';
 const resend = new Resend ('re_9dz9Fsnr_6nKWMn9U4mX7khQ99bKdJ7t6')
+import { Kafka } from 'kafkajs';
+
+const kafka = new Kafka ({ 
+    clientId : 'my-app' , 
+    brokers : ['localhost:9092']
+})
+const producer = kafka.producer(); 
+async function  ConnectPkafka() {
+    await producer.connect();
+    await producer.send({
+        topic: "Q1",
+        messages: [{
+        value: "connection success"
+        }]
+    });
+}
+ConnectPkafka(); 
+let waitforCreateReqId :number;
+let waitforCloseReqId : number;
+const consumer = kafka.consumer({groupId : 'group-3'}); 
+
+async function connectCkafka() {
+  await consumer.connect();
+  await consumer.subscribe({
+    topic: "Q2", fromBeginning: true
+  })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      try { 
+        //@ts-ignore
+        const data = JSON.parse(message?.value?.toString())
+        console.log("data" + JSON.stringify(data))
+        if(data.state == 'open'){
+            waitforCreateReqId = data.reqId
+        }
+        if(data.state == 'closed'){ 
+            waitforCloseReqId = data.reqId
+        }
+        console.log('reqId =' + waitforCreateReqId)
+      }  
+      catch(err) { 
+        console.error("hehe err" + err )
+      }
+      console.log({
+        offset: message.offset,
+        value: message?.value?.toString(),
+      })
+    },
+  })
+}
+
+
+connectCkafka();
+
 
 const JWT_PASSWORD = "HELLO BHAIYA !"
 import Cookies from 'js-cookie';
 
-const client = createClient()
-await client.connect()
 app.use(express.json()) 
 const myHeaders = new Headers(); // Currently empty
 
@@ -104,20 +155,51 @@ app.post('/api/v1/trade/create' , async (req , res)=> {
     const token = req.headers.authorization;
     let payload = req.body; 
     
-    if(token){
-        const user = jwt.verify(token,JWT_PASSWORD)
-        if(user){ 
+    if(token || 1==1){
+        // const user = jwt.verify(token,JWT_PASSWORD)
+        let user
+        if(user || 1==1){ 
             //post a request to engine be
             //brpop
+            const reqId = Date.now()+Math.random()
             try{ 
-                await client.lPush("submissions" , JSON.stringify({
-                asset : payload.asset , 
-                type : payload.type , 
-                margin : payload.margin , 
-                leverage : payload.leverage , 
-                slippage : payload.slippage
-             }))
-                res.json("Submissions received")
+                await producer.send({
+                    topic: "Q1",
+                    messages: [{
+                        value: JSON.stringify({ type : 'trade' ,  trade : {
+                            reqId : reqId ,
+                            asset : payload.asset , 
+                            type : payload.type , 
+                            margin : payload.margin , 
+                            leverage : payload.leverage , 
+                            slippage : payload.slippage}
+                            
+                        })
+                    }]
+                });
+                const myPromise = new Promise ((resolve , reject) => { 
+                    const start = Date.now()
+                    function poll ( ){ 
+                        if(waitforCreateReqId == reqId){ 
+                            let tt = Date.now() - start
+                            resolve("successfull , time taken :  "  + tt )
+                        }
+                        else if(Date.now()-start > 3000){
+                            reject("failed hogis ")
+                        }
+                        else { 
+                            setTimeout(poll , 5)
+                        }
+                    }
+                    poll() ;   
+                })
+                myPromise.then((data)=> { 
+                    res.json("details :  " + JSON.stringify(data))
+                }).catch((err)=> { 
+                    res.json("didnt worked bhai" + err)
+                })
+                
+                
             }catch(err){ 
                 res.json("something went wrong")
             }
@@ -127,14 +209,81 @@ app.post('/api/v1/trade/create' , async (req , res)=> {
 })
 app.post('/api/v1/trade/close' , async (req , res)=> { 
     const payload = req.body;
-    let orderId = payload.orderId
+    let orderId = payload.orderId; 
+    let reqId = Math.trunc(Date.now() + Math.random())
+    try{ 
+        await producer.send({
+            topic: "Q1",
+            messages: [{
+                value: JSON.stringify({ type : 'closeOrder' ,  trade : {
+                        orderId : orderId,
+                        reqId : reqId
+                    }   
+                })
+            }]
+        });
+        const promise = new Promise((resolve, reject) => {
+            const start = Date.now()
+            function poll() { 
+                if (waitforCloseReqId == reqId){ 
+                    const tt = Date.now() - start;
+                    resolve("yaay it worked" + tt) 
+                }
+                else if((Date.now()-start)>5000){ 
+                    reject("failed hogis")
+                }
+                else{ 
+                    setTimeout(poll , 10)
+                }
+            }
+            poll()
+        })
+        promise.then((data)=> {
+            res.json("details" + JSON.stringify(data))
+        }).catch((err)=>{ 
+            res.status(403).json("failed" + err)
+        })
+        
+    }catch(err){ 
+        res.json("something went wrong")
+    }
+
     // axios.post('https://localhost:3000')
 })
 app.get('/api/v1/balance/usd' , async (req , res)=> { 
-    
+    try { 
+        await producer.send({ 
+            topic : 'Q1' , 
+            messages : [{ 
+                value : JSON.stringify({
+                    type : 'getBalance'
+                })
+            }]
+        })
+        res.json("ur data is $20000")
+    }
+    catch(err) { 
+
+        console.error("something went Wrong => " + err)
+    }
 })
 app.get('/api/v1/supportedAssets' , async (req , res)=> { 
-    
+    try { 
+        await producer.send({ 
+            topic : 'Q1' , 
+            messages : [{ 
+                value : JSON.stringify({
+                    type : 'supportedAssets'
+                })
+            }]
+        })
+        
+        res.json("ur data is $20000")
+    }
+    catch(err) { 
+
+        console.error("something went Wrong => " + err)
+    }
 })
 
 
